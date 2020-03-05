61845daeeed885394200a49b0eb8cefe7b6c98ea
Stefan <stefanlastric@gmail.com>
2020-03-05 10:41:00 +0100

    database created on mongoAtlas and connected

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//Database connection
+connectDB();
+
 app.get('/', (req, res) => res.send('API running'));
 const PORT = process.env.PORT || 5000;
 
